6ec09d933036b218daaee051471e1db4799485cf
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-11-30 17:25:51 -0300

    adds instructions and basic translation template

--- include/Translator.h
+++ include/Translator.h
@@ -2,6 +2,7 @@
 #define TRANSLATOR_H
 
 #include <iostream>
+#include <unordered_map>
 #include "../include/File.h"
 
 using namespace std;
@@ -12,14 +13,45 @@ typedef struct {
 
 typedef struct {
     unsigned short int args, size;
-    string name;
 } Directive;
 
 class Translator {
 private:
-    File* program;
+    unordered_map<string, vector<string>> program;
+
+    unordered_map<string, Instruction> Instructions = {
+        {"ADD",     (Instruction) {1, 1}},
+        {"SUB",     (Instruction) {1, 2}},
+        {"MULT",    (Instruction) {1, 3}},
+        {"DIV",     (Instruction) {1, 4}},
+        {"JMP",     (Instruction) {1, 5}},
+        {"JMPN",    (Instruction) {1, 6}},
+        {"JMPP",    (Instruction) {1, 7}},
+        {"JMPZ",    (Instruction) {1, 8}},
+        {"COPY",    (Instruction) {2, 9}},
+        {"LOAD",    (Instruction) {1, 10}},
+        {"STORE",   (Instruction) {1, 11}},
+        {"INPUT",   (Instruction) {1, 12}},
+        {"OUTPUT",  (Instruction) {1, 13}},
+        {"C_INPUT", (Instruction) {1, 15}},
+        {"C_OUTPUT",(Instruction) {1, 16}},
+        {"S_INPUT", (Instruction) {2, 19}},
+        {"S_OUTPUT",(Instruction) {2, 20}},
+        {"STOP",    (Instruction) {0, 14}}
+    };
+
+    unordered_map<string, Directive> Directives = {
+        {"SECTION", (Directive) {1, 0}},
+        {"SPACE",   (Directive) {0, 1}},
+        {"CONST",   (Directive) {1, 1}},
+        {"EQU",     (Directive) {1, 0}},
+        {"IF",      (Directive) {1, 0}},
+
+    };
+
+    string toIA32(string);
 public:
-    Translator();
+    Translator(unordered_map<string, vector<string>>);
     ~Translator();
 
     void translate();
--- src/Translator.cpp
+++ src/Translator.cpp
@@ -1,1 +1,64 @@
 #include "../include/Translator.h"
+#include "../include/Helper.h"
+
+Translator::Translator(unordered_map<string, vector<string>> preprocessedFile) {
+    this->program = preprocessedFile;
+}
+
+Translator::~Translator() {
+}
+
+void Translator::translate() {
+    vector<string> translatedProgram;
+
+    /* Section Data */
+    /* Section Bss */
+    /* Section Text */
+    for (auto line : program["text"]) {
+        translatedProgram.push_back(this->toIA32(line));
+    }
+}
+
+string Translator::toIA32(string line) {
+    auto lineContents = split(line, ' ', '\t');
+
+    Line *l = getLineElements(lineContents);
+
+    if (l->operation == "ADD") {
+
+    } else if (l->operation == "SUB") {
+
+    } else if (l->operation == "MULT") {
+        
+    } else if (l->operation == "DIV") {
+        
+    } else if (l->operation == "JMP") {
+        
+    } else if (l->operation == "JMPP") {
+        
+    } else if (l->operation == "JMPZ") {
+        
+    } else if (l->operation == "COPY") {
+        
+    } else if (l->operation == "LOAD") {
+        
+    } else if (l->operation == "STORE") {
+        
+    } else if (l->operation == "INPUT") {
+        
+    } else if (l->operation == "OUTPUT") {
+        
+    } else if (l->operation == "C_INPUT") {
+        
+    } else if (l->operation == "C_OUTPUT") {
+        
+    } else if (l->operation == "S_INPUT") {
+        
+    } else if (l->operation == "S_OUTPUT") {
+        
+    } else if (l->operation == "SPACE") {
+        
+    } else if (l->operation == "CONST") {
+        
+    } 
+}
