c7f3de026aa3c8a4f3b84a5a593361626fd7fe93
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-12-01 15:44:29 -0300

    assemble and link

--- src/tradutor.cpp
+++ src/tradutor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/wait.h>
 #include <string.h>
 #include "../include/PreProcessor.h"
 #include "../include/Translator.h"
@@ -17,14 +18,29 @@ int main(int argc, char const *argv[]) {
         return 0;
     }
 
+    /* Get program filename */
+    string file = programFilepath.substr(programFilepath.find_last_of("/") + 1);
+    string programFilename = file.substr(0, file.find_last_of('.'));
+
+    /* Pre processing stage */
     PreProcessor* preProcessor = new PreProcessor(programFilepath, false);
 
     auto preProcessedFile = preProcessor->preProcess();
 
+    /* Translation stage */
     Translator* translator = new Translator(preProcessedFile);
 
     translator->translate();
-    translator->printToFile(programFilepath);
+    translator->printToFile(programFilename);
 
+    cout << "Montando programa: " << endl;
+
+    string command = "nasm -f elf32 -o " + programFilename + ".o " + programFilename + ".s";
+    cout << command << endl;
+    int exit = system(command.c_str());
+    
+    command = "ld -m elf_i386 -o " + programFilename + " " + programFilename + ".o";
+    cout << command << endl;
+    exit = system(command.c_str());
     return 0;
 }
